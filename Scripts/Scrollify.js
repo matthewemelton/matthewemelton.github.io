2743ec88243093c35c8b4e6111f218ddb318db73
matthewemelton <90717391+matthewemelton@users.noreply.github.com>
2022-09-24 17:55:02 -0400

    Update Scrollify.js

--- Scripts/Scrollify.js
+++ Scripts/Scrollify.js
@@ -19,7 +19,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 $(document).ready(function(){
-	if($(window).width < 768){
+	if($(window).width <= 768){
 		$.scrollify.disable();
 	}
 		$.scrollify({
